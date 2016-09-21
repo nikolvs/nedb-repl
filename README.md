d99d60fcb9005c7092be6629cbea3125d8a401ce
Nikolas Silva <nikolas.silva@westwing.com.br>
2016-09-21 12:19:27 -0300

    Initial commit
    Create simple repl structure
    
    Turn Datastore methods synchronous
    
    Put VM in REPL context
    
    Add .open command
    
    Allow multi-line input
    
    Add timeout option
    
    Set timeout alias
    
    Set custom version output
    
    Set custom version output
    
    Change .open output
    
    Add REPL writer
    
    Rewrite the REPL writer
    
    Use string interpolation instead of concatenation
    
    Use toString() and chalk.red to display errors
    
    Add REPL intro
    
    Add more recoverable errors
    
    Improve writer
    
    Improve .open output
    
    Use array instead of object to list recoverable errors
    
    Improve writer for functions
    
    Add REPL events
    
    Use String() instead of .toString()
    
    Use circular-json to securely stringify JSON
    
    Setup travis
    
    Update README

---
+++ README.md
@@ -0,0 +1,21 @@
+# nedb-repl
+> The command-line tool for NeDB
+
+![](terminal.gif)
+
+[![Build Status](https://travis-ci.org/nikolassilva/nedb-repl.svg)](https://travis-ci.org/nikolassilva/nedb-repl)
+[![Dependency Status](https://david-dm.org/nikolassilva/nedb-repl.svg)](https://david-dm.org/nikolassilva/nedb-repl)
+[![devDependency Status](https://david-dm.org/nikolassilva/nedb-repl/dev-status.svg)](https://david-dm.org/nikolassilva/nedb-repl#info=devDependencies)
+
+## Install
+```bash
+npm install -g nedb-repl
+```
+
+## Usage
+```bash
+$ nedb --help
+```
+
+## License
+MIT © [Nikolas Silva](http://nikolas.com.br)
---
+++ test/datastore.test.js
@@ -0,0 +1,83 @@
+import test from 'ava';
+import datastore from '../lib/datastore';
+
+const db = datastore();
+const data = [
+  { a: 1 },
+  { a: 2 },
+  { a: 3 },
+];
+
+test('should insert a document', (t) => {
+  const doc = db.insert(data[0]);
+
+  t.is(doc.a, data[0].a);
+  t.true('_id' in doc);
+});
+
+test('should insert two documents', (t) => {
+  const arr = data.slice(1);
+  const docs = db.insert(arr);
+
+  docs.forEach((doc, index) => {
+    t.is(doc.a, arr[index].a);
+    t.true('_id' in doc);
+  });
+});
+
+test('should find all documents', (t) => {
+  const docs = db.find({}).exec();
+
+  if (docs.length !== data.length) {
+    t.fail();
+  }
+
+  docs.forEach((doc) => {
+    const i = data.find(d => d.a === doc.a);
+    t.not(i, null);
+  });
+});
+
+test('should find all documents keeping only the `a` field', (t) => {
+  const docs = db
+    .find({}, { a: 1, _id: 0 })
+    .sort({ a: 1 })
+    .exec();
+
+  t.deepEqual(docs, data);
+});
+
+test('should find one document', (t) => {
+  const doc = db.findOne({ a: 3 }).exec();
+  t.is(doc.a, data[2].a);
+});
+
+test('should count documents', (t) => {
+  const count = db.count({}).exec();
+  t.is(count, data.length);
+});
+
+test('should update a document', (t) => {
+  const countUpdates = db.update({ a: 1 }, { a: 0 });
+  t.is(countUpdates, 1);
+});
+
+test('should update documents w/ `a` field greater than 1', (t) => {
+  const countUpdates = db.update(
+    { a: { $gt: 1 } },
+    { a: 4 },
+    { multi: true }
+  );
+
+  t.is(countUpdates, data.filter(d => d.a > 1).length);
+});
+
+test('should remove a document', (t) => {
+  const countRemoved = db.remove({ a: 0 });
+  t.is(countRemoved, 1);
+});
+
+test('should remove all documents', (t) => {
+  const countRemoved = db.remove({}, { multi: true });
+  t.is(countRemoved, data.length - 1);
+});
